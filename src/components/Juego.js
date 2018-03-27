5c714ff716381035b357126a8a2f5f0f54628275
Flor <florenciaz@gmail.com>
2018-03-27 20:53:40 -0300

    Clean comments. Add xp points when the player beats a monster. Show xp points and player's level on the screen.

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -5,7 +5,7 @@ import Mapa from './Mapa';
 import GameEnd from './GameEnd';
 import TableroPuntaje from './TableroPuntaje';
 
-// Generador de Nivel:
+// Generador de Nivel de Juego:
 const FILAS = 10;
 const COLUMNAS = 15;
 const ALTURA_FILA = 40;
@@ -13,22 +13,19 @@ const ANCHO_COL = 40;
 const CANT_SALIDAS = 1;
 const CANT_BOSS = 1;
 const CANT_MONSTRUOS = 10;
-const CANT_COMIDA = 8;
-const CANT_ARMAS = 4; // del jugador
+const CANT_COMIDA = 20;
+const CANT_ARMAS = 4; // para distribuir por el mapa y que pueda recoger el jugador
 const CANT_PAREDES = 15;
-//const SIZE_WALL = 3; // cantidad máxima de celdas de una pared.
 const POS_INICIO_JUGADOR = { row: 0, col: 0};
+
 const FOOD_ENERGY = 10;
-// const MONSTER_POWER = 50;
 const MONSTER_ENERGY = 50;
 const MONSTER_LEVEL = 1;
 const BOSS_LEVEL = 5;
 const BOSS_ENERGY = 80;
-const ARMAS_JUGADOR = {"estrella": 10, "libro": 20};
+const ARMAS_JUGADOR = {"estrella": 5, "libro": 10};
 const ARMAS_MONSTRUOS = {"martillo": 5, "aguja": 1};
-// 
-// const MONSTRUO = {energia: MONSTER_POWER, arma: "martillo", nivel: 1}; // por ahora queda así.
-//
+
 let posiciones_disponibles = FILAS * COLUMNAS;
 
 const MAPA = createMapa(FILAS, COLUMNAS);
@@ -112,7 +109,8 @@ function isOnTheMap(mapa, pos) {
   }
 
 function agregarParedAlMapa(mapa, filas, columnas) {
-  // TO DO: Revisar y reescribir. Por ahora está fijo que por cada pared se van a elegir 3 posiciones.
+  // TO DO: Revisar.
+  // Por ahora está fijo que por cada pared se van a elegir 3 posiciones.
 
   for (let i = 0; i < CANT_PAREDES; i++) {
     const pos_1 = choosePos(filas, columnas); // elegir la del medio
@@ -127,7 +125,7 @@ function agregarParedAlMapa(mapa, filas, columnas) {
 
 function ocupada(mapa, pos) {
   // Se va a considerar desocupada si en mapa[pos.row][pos.col] hay null
-  return Boolean(mapa[pos.row][pos.col]); // null castea a false, y una cadena no vacía castea a true
+  return Boolean(mapa[pos.row][pos.col]); // null castea a false.
 }
 
 function inicializarMapa(filas, columnas) {
@@ -163,6 +161,7 @@ function addExitToMap(mapa, pos) {
 }
 
 function addMonsterToMap(mapa, pos) {
+  // TO DO
   // Elegir un número al azar entre 1 y 4 para definir el nivel de los monstruos
   // Por ahora, van a tener todos nivel 1.
   addItemToMap(mapa, {type: "monster", energia: MONSTER_ENERGY, arma: "aguja", nivel: MONSTER_LEVEL}, pos);
@@ -200,20 +199,12 @@ function randomPositiveIntegerFromMinToMax(min, max) {
   return Math.floor(Math.random() * (max - min) + min); 
 }
 
-/*
-function lanzarMoneda() {
-  // Devuelve 0 o 1, al azar.
-  return randomPositiveIntegerFromMinToMax(0, 2); // usar la general y pasarle 0 y 2 como argumentos
-}
-*/
-
-
 class Juego extends React.Component {
   constructor(){
     super();
     this.state = {
       jugador: {pos: POS_INICIO_JUGADOR},
-      puntos: 0,
+      xp: 0,
       energia: 50,
       arma: "estrella",
       nivelJugador: 1,
@@ -261,7 +252,6 @@ class Juego extends React.Component {
   updateState(pos) {
     // Antes de querer acceder a type en cada una de estas funciones, veo si pos es null
     const celda = this.getCellObj(pos);
-    // console.log(celda);
     if(celda){
       if (this.isFood(pos)){
         this.eatFood(pos);
@@ -296,6 +286,16 @@ class Juego extends React.Component {
     this.setState({puntos: this.state.puntos + amount});
   }        
 
+  addXP(amount){
+    const xp = this.state.xp + amount;
+    if (xp >= 25) {
+      const nivel = this.state.nivelJugador + 1;
+      this.setState({xp: 0, nivelJugador: nivel});  
+    } else {
+      this.setState({xp: xp});
+    }
+  }        
+
   removeFromMap(pos) {
     const mapa = [...this.state.mapa];
     mapa[pos.row][pos.col] = null;
@@ -304,68 +304,61 @@ class Juego extends React.Component {
     })
   }
 
+  // Más adelante adecuar el enfrentamiento a las especificaciones. Ver user stories.
+  fightMonster(pos){
+    // TO DO
+    const xp = (this.isTheBoss(pos))? 20 : 5;
+
+    const playerState = {energia: this.state.energia, arma: this.state.arma, nivel: this.state.nivelJugador};
+    const monsterState = this.getCellObj(pos); // el monstruo de la posición actual
+    console.error("Energía del monstruo: " + monsterState.energia);
+    
+    const damageByMonster = ARMAS_MONSTRUOS[monsterState.arma] + monsterState.nivel;
+    const damageByPlayer = ARMAS_JUGADOR[playerState.arma] + playerState.nivel;
+
+
+    // Acá se va a calcular quién recibe el daño.
+    // Al que le toca perder se le reduce su energía.
+    // Si el monstruo se queda sin energía, desaparece del mapa, y el jugador gana experiencia
+    // Si acumula cierta cantidad de puntos de experiencia suma uno a su nivel,
+    // y el conteo de experiencia para llegar al próximo nivel puede ponerse a 0 o seguir acumulándose.
+
+    const winner = this.attack(playerState, monsterState);
+
+    if (winner === "player"){
+      this.reduceMonsterEnergy(pos, damageByPlayer);
+    } else {
+      // restar puntos al jugador
+      this.reduceEnergy(damageByMonster);
+    }
+
+    if (monsterState.energia <= 0) {  
+      if(this.isTheBoss(pos)){
+        // Esto primero porque si no, no detecta nunca que es el boss porque parece que lo saca del mapa y mueve al jugador entonces la posición es otra...
+        this.setState({bossCaptured: true});
+      }
+      this.removeFromMap(pos);
+      this.addXP(xp); //puntos de experiencia que se suma al jugador
+      this.moveTo(pos);
+    }
+  }
+
   attack(player, monster){
     // player y monster son de la forma: {energia: Number, arma: String, nivel: Number}
-    // Devuelve un "player" o "monster" según quién haya ganado el ataque.
+    // Devuelve "player" o "monster" según quién haya ganado el ataque.
     
-    // usar random y algo más para que las probabilidades se ajusten al power de cada uno. 
     // Provisoriamente:
     const playerPower = (player.energia + ARMAS_JUGADOR[player.arma] + player.nivel) * Math.random();
     const monsterPower = (monster.energia + ARMAS_MONSTRUOS[monster.arma] + monster.nivel) * Math.random();
     console.log("Player power: " + playerPower);
     console.log("Monster power: " + monsterPower);
     console.log("Monster energy: " + monster.energia);
-    // acá veo cuál gana.
-
-    // por ahora, y va a cambiar porque no tiene que ser así:
+    // Por ahora gana el que sacó mayor puntaje.
     return (playerPower > monsterPower)? "player" : "monster";
   }
 
-  // Más adelante adecuar el enfrentamiento a las especificaciones. Ver user stories.
-
-  fightMonster(pos){
-    // TO DO
-    // provisorio.
-    // falta implementar que el jugador gane puntos de experiencia y con eso pueda ir subiendo su nivel.
-
-    const playerState = {energia: this.state.energia, arma: this.state.arma, nivel: this.state.nivelJugador};
-    const monsterState = this.getCellObj(pos); // el monstruo de la posición actual pos
-    console.error("Energía del monstruo: " + monsterState.energia);
-    // acá se va a calcular quién recibe el daño.
-    // al que le toca perder se le reduce su energía.
-    // si el monstruo se queda sin energía, desaparece del mapa, y el jugador gana experiencia
-    // si acumula cierta cantidad de puntos de experiencia suma uno a su nivel, y el conteo de experiencia para llegar al próximo nivel puede ponerse a 0 o seguir acumulándose.
-
-    // La función podría devolver "monster" o "player", según quién ganó el ataque.
-    // entonces si ganó player, tal cosa, else, tal otra.
-
-    const winner = this.attack(playerState, monsterState);
-
-    if (winner === "player"){
-      this.reduceMonsterEnergy(pos, 5); // por ahora queda así
-    } else {
-      // restar puntos al jugador
-      this.reduceEnergy(5); // por ahora queda así
-    }
-
-    if (monsterState.energia <= 0) {  
-      if(this.isTheBoss(pos)){
-        // esto primero porque si no, no detecta nunca que es el boss porque parece que lo saca del mapa y mueve al jugador entonces la posición es otra...
-        this.setState({bossCaptured: true});
-      }
-
-      // Esto debería suceder si el monstruo perdió toda su energía. Es decir si el jugador venció al monstruo.
-      this.removeFromMap(pos);
-      // this.addPoints(enemyPower); // acá debería indicarse agregar puntos de experiencia al jugador
-      this.moveTo(pos);
-      // ¿Cómo lo puedo escribir mejor?
-    }
-
-    // ¿acá tengo que chequear si es el monster boss y actualizar el estado de bossCaptured?
-
-  }
-
   reduceEnergy(amount) {
+    //Para el jugador
     this.addEnergy((-amount));
   }
 
@@ -392,12 +385,6 @@ class Juego extends React.Component {
     return false;
   }
 
-  // ¿Es necesaria isEmptySpace()? Me parece que no
-  /* isEmptySpace(pos) {
-    return !ocupada(this.state.mapa, pos); // función fuera de la clase.
-  }
-  */
-
   isFood(pos) {
     return this.getCellObjType(pos) === "food";
   }
@@ -416,8 +403,6 @@ class Juego extends React.Component {
   }
 
   isLevelCompleted(){
-    // Criterio actual, provisorio: se termina el nivel si el jugador no tiene más energía o si llegó a la celda marcada como salida. ¿Qué pasa si no puede porque está encerrada por paredes o el mismo jugador está encerrado? Por ahora, volver a cargar la página para generar otro mapa.
-    // Agregar el criterio de que tiene que haber vencido al monstruo jefe.
     let res = (this.isTheExit(this.state.jugador.pos) && this.state.bossCaptured) || this.state.energia <= 0;
     return res;
   }
@@ -443,7 +428,6 @@ class Juego extends React.Component {
   }
 
   render() {
-    // Si el nivel no está completado entonces se renderea el mapa, y si está completado se renderea un mensaje que indica que se terminó el juego.
     let fin_o_mapa = <Mapa darkOn={this.state.darkOn} mapa={this.state.mapa} ancho={this.state.ancho} alto={this.state.alto} jugadorPos={this.state.jugador.pos} />;
     if (this.isLevelCompleted()) {
       fin_o_mapa = <GameEnd message="Game finished!" />;
@@ -453,9 +437,10 @@ class Juego extends React.Component {
           <Instrucciones />
           <button className="toggleDark" onClick={this.toggleDarkOn.bind(this)}>Toggle Darkness</button>
           <TableroPuntaje 
-            puntos={this.state.puntos}
+            xp={this.state.xp}
             energia={this.state.energia}
             arma={this.state.arma}
+            nivelJugador={this.state.nivelJugador}
           />
           {fin_o_mapa}
         </div>
@@ -489,12 +474,11 @@ function copyRow(board, r, cols) {
   let row = [];
     for (let c = 0; c < cols; c++) {
       const contenido = board[r][c];
-      const obj = (contenido) ? Object.assign({}, contenido) : null; // acá el problema se da cuando quiere copiar una cadena
-      // Hay un problema cuando board[r][c] es null, en ese caso obj va a ser {} y no null, y entonces los métodos y funciones que cuentan con que haya null o un objeto no vacío fallan
-      // porque Boolean(null) es false pero ¡Boolean({}) es true!.
-      // se me ocurre que podría chequear acá si obj es {} y en ese caso devolver null o cambiar en otro lado, pero si cambio acá me parece más sencillo y mantiene la interfaz de devolver null cuando había null
-      /* O chequeo antes la celda es de tipo objeto o hago que todo lo que va en el mapa sea por lo menos de tipo {type: algúnValor} */
-      
+      const obj = (contenido) ? Object.assign({}, contenido) : null;
+      // Cuando board[r][c] es null, si no chequeo y uso assign, obj va a ser {} y no null,
+      // y entonces los métodos y funciones que cuentan con que haya null o un objeto no vacío
+      // fallan porque Boolean(null) es false pero ¡Boolean({}) es true!.
+      // Por eso chequeo.
       row.push(obj);
     }  
   return row;
